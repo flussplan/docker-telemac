833bfd57dea3d8c66647d98d190e93dd5ad26ef0
Peter Steiner <peter.steiner@flussplan.at>
2020-04-23 21:42:33 +0200

    custom build 3rd parties

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -2,10 +2,18 @@
 
 export HOMETEL="${TELEMAC_ROOT}/${TELEMAC_MASCARET_VER}"
 export SYSTELCFG="${TELEMAC_ROOT}/systel.cfg"
-export USETELCFG="debgfopenmpi"
+export USETELCFG="${TELEMAC_TARGET}"
 export PYTHONUNBUFFERED="true"
-export LD_LIBRARY_PATH="${HOMETEL}/builds/${USETELCFG}/wrap_api/lib:${LD_LIBRARY_PATH}"
 export PYTHONPATH="${HOMETEL}/builds/${USETELCFG}/wrap_api/lib:$PYTHONPATH"
+
+VENDOR_HOME="${TELEMAC_ROOT}/vendor"
+
+export HDF5HOME="${VENDOR_HOME}/hdf5"
+export MEDHOME="${VENDOR_HOME}/med"
+export METISHOME="${VENDOR_HOME}/metis"
+
+export PATH="${HDF5HOME}/bin:${MEDHOME}/bin:${METISHOME}/bin:${PATH}"
+export LD_LIBRARY_PATH="${HOMETEL}/builds/${USETELCFG}/wrap_api/lib:${HDF5HOME}/lib:${MEDHOME}/lib:${LD_LIBRARY_PATH}"
 
 TELEMAC_MAJOR_VER=$(echo $TELEMAC_MASCARET_VER | cut -c 1-2)
 if [[ $TELEMAC_MAJOR_VER == "v7"  ]]; then
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,22 +1,76 @@
 #!/bin/bash -ev
 
+# debian dependencies + python libs
+
+TELEMAC_MAJOR_VER=$(echo $TELEMAC_MASCARET_VER | cut -c 1-2)
+if [[ $TELEMAC_MAJOR_VER == "v7"  ]]; then
+    PYTHON_PKGS="python python-pip"
+    PIP_PKGS="numpy matplotlib==2.0.2 scipy jupyter"
+    export PYTHON="python"
+        
+    sed -i 's/<partel.par>/PARTEL.PAR/' ${TELEMAC_ROOT}/systel.cfg
+else
+    PYTHON_PKGS="python3 python3-pip"
+    PIP_PKGS="numpy matplotlib scipy jupyter"
+    export PYTHON="python3"
+    export PYTHON_VERSION=3
+fi
+
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y
 apt-get -o Dpkg::Options::="--force-confold" install -q -y \
-    libopenmpi-dev openmpi-bin gfortran subversion \
-    libmetis-dev curl apache2 php apache2 libapache2-mod-php
+    libopenmpi-dev openmpi-bin gfortran subversion less \
+    cmake swig vim curl zlib1g-dev ${PYTHON_PKGS}
 
-TELEMAC_MAJOR_VER=$(echo $TELEMAC_MASCARET_VER | cut -c 1-2)
-if [[ $TELEMAC_MAJOR_VER == "v7"  ]]; then
-    apt-get -o Dpkg::Options::="--force-confold" install -q -y \
-        python python-pip
-    python -m pip install numpy matplotlib==2.0.2 scipy
+${PYTHON} -m pip install ${PIP_PKGS}
 
-    sed -i 's/<partel.par>/PARTEL.PAR/' ${TELEMAC_ROOT}/systel.cfg
-else
-    apt-get -o Dpkg::Options::="--force-confold" install -q -y \
-        python3 python3-pip
-    python3 -m pip install numpy matplotlib scipy
-fi
+# vendor libs
+
+echo "Compiling vendor libs..."
+
+cd /tmp
+
+VENDOR_HOME="${TELEMAC_ROOT}/vendor"
+
+METISHOME=${VENDOR_HOME}/metis
+METIS_REL=metis-5.1.0
+
+HDF5HOME=${VENDOR_HOME}/hdf5
+HDF_REL=hdf5-1.10.6
+
+MEDHOME=${VENDOR_HOME}/med
+MED_REL=med-4.0.0
+
+# METIS
+curl -Ls http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS_REL}.tar.gz | tar xvz
+cd ${METIS_REL}
+make config prefix=${METISHOME} openmp=set
+make
+make install
+cd ..
+rm -rf ${METIS_REL}
+
+# HDF5 (current MED requires >= 1.10.2 and < 1.11)
+HDF_MIN_REL=$(echo ${HDF_REL} | sed 's/\.[^.]*$//')
+
+curl -Ls https://support.hdfgroup.org/ftp/HDF5/releases/${HDF_MIN_REL}/${HDF_REL}/src/${HDF_REL}.tar.gz | tar xvz
+cd ${HDF_REL}
+./configure --prefix=${HDF5HOME} --enable-build-mode=production --enable-fortran --disable-tests \
+            --disable-static --enable-optimization=high --enable-parallel
+make
+make install
+cd ..
+rm -rf ${HDF_REL}
+
+# MED
+curl -Ls http://files.salome-platform.org/Salome/other/${MED_REL}.tar.gz | tar xvz
+cd ${MED_REL}
+./configure --prefix=${MEDHOME} --disable-static --with-hdf5=${HDF5HOME} --with-swig=yes
+make
+make install
+cd ..
+rm -rf ${MED_REL}
+
+echo "Finished $0"
